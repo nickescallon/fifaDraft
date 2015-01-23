86f4b81d27c576c65fe5ec229b2703da1a574eef
Nick Escallon <nickescallon@gmail.com>
2015-01-22 20:50:31 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var http = require('http');
+
+var app = express();
+
+app.set('port', process.env.PORT || 3000);
+
+// Servce static assets
+app.use('/',  express.static(__dirname ));
+// app.use('/images',  express.static(__dirname + '/public/images'));
+
+// Routes - catch-all to return index.html
+app.get('*', function (req, res) {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+var server = http.createServer(app);
+server.listen(app.get('port'), function() {
+  console.log('app listenting on port: ' + app.get('port'));
+});
